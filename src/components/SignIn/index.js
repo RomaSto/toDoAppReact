0b8be9c8467743b81c5c23c0c50e89c4b89ca64e
Roman Storozhuk <roman.storozhuk@perfectial.com>
2018-02-09 16:26:24 +0200

    First commit

---
+++ src/components/SignIn/index.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { FirebaseAuth } from 'react-firebaseui';
+import  * as firebase from 'firebase';
+
+import { SignUpLink } from '../SignUp';
+import { PasswordForgetLink } from '../PasswordForget';
+import * as authFunctions from '../../firebase/auth';
+import * as routes from '../../constants/routes';
+
+const SignInPage = ({ history }) => {
+
+  // Configure FirebaseUI
+  const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: '/home',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ]
+  };
+  return <div>
+    <h1>SignIn</h1>
+    <SignInForm history={history} />
+    <PasswordForgetLink />
+    <SignUpLink />
+    <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+  </div>
+}
+
+const updateByPropertyName = (propertyName, value) => () => ({
+  [propertyName]: value,
+});
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null,
+};
+
+class SignInForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = (event) => {
+    const {
+      email,
+      password,
+    } = this.state;
+
+    const {
+      history,
+    } = this.props;
+
+    authFunctions.doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState(() => ({ ...INITIAL_STATE }));
+        history.push(routes.HOME);
+      })
+      .catch(error => {
+        this.setState(updateByPropertyName('error', error));
+      });
+
+    event.preventDefault();
+  }
+
+  render() {
+    const {
+      email,
+      password,
+      error,
+    } = this.state;
+
+    const isInvalid =
+      password === '' ||
+      email === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={email}
+          onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={password}
+          onChange={event => this.setState(updateByPropertyName('password', event.target.value))}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+
+        { error && <p>{error.message}</p> }
+      </form>
+    );
+  }
+}
+
+export default withRouter(SignInPage);
+
+export {
+  SignInForm,
+};
---
+++ src/firebase/firebase.js
@@ -0,0 +1,41 @@
+import * as firebase from 'firebase';
+
+const API_KEY = "AIzaSyB3wXXsXZ-aUc0N5Jb3SvHyD-4P0WVshX4";
+const AUTH_DOMAIN = "react-to-do-141c4.firebaseapp.com";
+const DATABASE_URL = "https://react-to-do-141c4.firebaseio.com";
+const PROJECT_ID = "react-to-do-141c4";
+const MESSAGING_SENDER_ID = "914758193361";
+
+const prodConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: MESSAGING_SENDER_ID,
+};
+
+const devConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: MESSAGING_SENDER_ID,
+};
+
+const config = process.env.NODE_ENV === 'production'
+  ? prodConfig
+  : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const db = firebase.database();
+const auth = firebase.auth();
+
+export {
+  db,
+  auth,
+};
